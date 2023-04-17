ef5578ddf89a13467f37537ed96ea9d04cdeaf08
chicohendrix <ritviksk@gmail.com>
2023-04-16 23:10:03 -0400

    Added Contain Test

--- src/BinarySearchTreeTest.java
+++ src/BinarySearchTreeTest.java
@@ -26,5 +26,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
             assertEquals("ID: 1 NUM_SONGS: 10 -- [Artist 1, Artist 2], ID: 2 NUM_SONGS: 5 -- [Artist 1, Artist 2], N, N, N", tree.toString());
         }
+        @Test
+        public void testContains() {
+            BinarySearchTree<Album> tree = new BinarySearchTree<>();
+            List<String> artists = Arrays.asList("Artist 1", "Artist 2");
+            tree.insert(new Album(1, artists, "Title 1", 10));
+            tree.insert(new Album(2, artists, "Title 2", 5));
+            tree.insert(new Album(3, artists, "Title 3", 15));
+
+            assertTrue(tree.contains(new Album(0, null, null, 10)));
+            assertFalse(tree.contains(new Album(0, null, null, 20)));
+        }
 
 }
