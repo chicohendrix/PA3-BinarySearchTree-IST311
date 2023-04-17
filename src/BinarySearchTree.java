b81d456948c497804509f1a9b8d99bdd655d6d88
chicohendrix <ritviksk@gmail.com>
2023-04-16 22:11:45 -0400

    Made Binary Search Tree Class

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    private Node<T> root;
+
+    private static class Node<T> {
+        T data;
+        Node<T> left;
+        Node<T> right;
+
+        Node(T data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
