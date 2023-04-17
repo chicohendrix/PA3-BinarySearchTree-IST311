c82621ef74ab69e46b72bd32767a5f5e205a6733
chicohendrix <ritviksk@gmail.com>
2023-04-16 23:08:09 -0400

    Added Insert and Delete Tests to the Test File

--- src/BinarySearchTreeTest.java
+++ src/BinarySearchTreeTest.java
@@ -1,5 +1,30 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
+    public class BinarySearchTreeTest {
+        @Test
+        public void testInsertAndToString() {
+            BinarySearchTree<Album> tree = new BinarySearchTree<>();
+            List<String> artists = Arrays.asList("Artist 1", "Artist 2");
+            tree.insert(new Album(1, artists, "Title 1", 10));
+            tree.insert(new Album(2, artists, "Title 2", 5));
+            tree.insert(new Album(3, artists, "Title 3", 15));
 
-class BinarySearchTreeTest {
+            assertEquals("ID: 1 NUM_SONGS: 10 -- [Artist 1, Artist 2], ID: 2 NUM_SONGS: 5 -- [Artist 1, Artist 2], N, N, ID: 3 NUM_SONGS: 15 -- [Artist 1, Artist 2], N, N", tree.toString());
+        }
+
+        @Test
+        public void testDelete() {
+            BinarySearchTree<Album> tree = new BinarySearchTree<>();
+            List<String> artists = Arrays.asList("Artist 1", "Artist 2");
+            tree.insert(new Album(1, artists, "Title 1", 10));
+            tree.insert(new Album(2, artists, "Title 2", 5));
+            tree.insert(new Album(3, artists, "Title 3", 15));
+
+            tree.delete(new Album(0, null, null, 15));
+
+            assertEquals("ID: 1 NUM_SONGS: 10 -- [Artist 1, Artist 2], ID: 2 NUM_SONGS: 5 -- [Artist 1, Artist 2], N, N, N", tree.toString());
+        }
 
 }
